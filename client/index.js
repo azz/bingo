13dab6eb157946e89fda2d57cd15fa0da9449b2b
Lucas Azzola <laazz1@student.monash.edu>
2016-03-26 19:59:42 +1100

    Boilerplate

---
+++ client/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+class Bingo extends React.Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return <div>Test</div>;
+    }
+}
+
+ReactDOM.render(<Bingo />, document.getElementById('react'));
